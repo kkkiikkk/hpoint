49848757070a94c95cecf08bfe94083f7c5e30c4
kkkiikkk <ruddnovskiy@gmail.com>
2023-03-10 00:48:57 -0800

    feat: update user model

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,17 +1,26 @@
 import mongoose from 'mongoose';
 
+export enum ACCOUNT_TYPE {
+	ORGANISATION = 'organisation',
+	VOLUNTEER = 'volunteer',
+	REFUGEE = 'refugee',
+}
+
 const userSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
+	country: String,
+	city_from: String,
+	account_type: { type: String, enum: ACCOUNT_TYPE, default: ACCOUNT_TYPE.REFUGEE },
 	password: {
 		type: String,
 		select: false
 	},
 	email: {
-		type: String, 
+		type: String,
 		index: true,
 		unique: true,
 	},
 }, { timestamps: true });
-	
+
 export const UserModel = mongoose.model('user', userSchema);
